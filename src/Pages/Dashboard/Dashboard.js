fa9eb960525e5a61f53a13368d1a120ad38de8b4
Nurul Islam Rimon <nurulislamrimon@gmail.com>
2022-09-26 01:17:03 +0600

    dashboard ready

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -7,16 +7,24 @@ const Dashboard = () => {
     return (
         <div className='mt-14 lg:mt-20 relative'>
             <div className="drawer drawer-mobile">
-                <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+                <input id="dashboard-drawer" type="checkbox" className="drawer-toggle" />
                 <div className="drawer-content flex flex-col items-center justify-center">
 
-                    <Outlet />             <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden absolute top-5 left-5">Open drawer</label>
+                    <Outlet />
+
+                    <label htmlFor="dashboard-drawer" className="btn btn-primary drawer-button lg:hidden absolute top-0 left-0 gap-1 flex flex-col">
+                        <div className="w-3 bg-success h-0.5"></div>
+                        <div className="w-3 bg-success h-0.5"></div>
+                    </label>
 
                 </div>
                 <div className="drawer-side">
-                    <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+                    <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
                     <ul className="menu p-4 overflow-y-auto w-80 bg-black glass text-success gap-5">
-                        <li><Link to='item1'>Sidebar Item 1</Link></li>
+
+                        <li>
+                            <Link to='item1'>Sidebar Item 1</Link>
+                        </li>
                         <li>
                             <button onClick={() => signOut(auth)} className='btn btn-neutral glass hover:btn-primary'>Logout</button></li>
                     </ul>
