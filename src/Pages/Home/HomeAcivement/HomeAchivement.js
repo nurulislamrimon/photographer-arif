6e284e9e7f04f5553dad2182f04503a25d2c5140
Nurul Islam Rimon <nurulislamrimon@gmail.com>
2023-10-21 21:16:50 +0600

    dynamic achivement added

--- src/Pages/Home/HomeAcivement/HomeAchivement.js
+++ src/Pages/Home/HomeAcivement/HomeAchivement.js
@@ -1,50 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./HomeAchivement.css";
+import { achivements } from "../../../Utilities/variables/achivements";
 
 const HomeAchivement = () => {
   return (
-    <div className="grid lg:grid-cols-2 gap-5 p-10">
-      <Link
-        to="/achivement"
-        className="relative hover:text-success lg:h-[40vw] cursor-pointer pb-16"
-      >
-        <img
-          data-aos="flip-right"
-          data-aos-easing="ease-out-cubic"
-          data-aos-duration="2000"
-          src="./images/achivements/arif-achivement-3.jpg"
-          alt="img"
-          className=" rounded-3xl h-full mx-auto "
-          loading="lazy"
-        />
-        <article className="absolute bottom-0 left-0 right-0 p-2 lg:p-5 flex flex-col glass rounded-bl-3xl rounded-br-3xl duration-1000">
-          <h3 className="text-3xl text-center">Achivement</h3>
-          <p className="text-lg text-center">
-            Some achivement gives us strength.
-          </p>
-        </article>
-      </Link>
-      <Link
-        to="/achivement"
-        className="relative hover:text-success lg:h-[40vw] cursor-pointer"
-      >
-        <img
-          data-aos="flip-left"
-          data-aos-easing="ease-out-cubic"
-          data-aos-duration="2000"
-          alt="img"
-          src="./images/achivements/arif-achivement-2.jpg"
-          className="rounded-3xl h-full  mx-auto"
-          loading="lazy"
-        />
-        <article className="absolute bottom-0 left-0 right-0 p-2 lg:p-5 flex flex-col glass rounded-bl-3xl rounded-br-3xl duration-1000">
-          <h3 className="text-3xl text-center">Achivement</h3>
-          <p className="text-lg text-center">
-            Some achivement gives us strength.
-          </p>
-        </article>
-      </Link>
+    <div className="grid lg:grid-cols-3 md:grid-cols-2 mt-5 justify-items-center justify-content-center gap-10 p-10">
+      {achivements.map((achivement) => (
+        <Link
+          key={achivement.id}
+          to="/achivement"
+          className="relative hover:text-success cursor-pointer"
+        >
+          <img
+            data-aos="flip-left"
+            data-aos-easing="ease-out-cubic"
+            data-aos-duration="2000"
+            alt="img"
+            src={achivement.photoUrl}
+            className="rounded-3xl h-auto mx-auto max-w-sm"
+            loading="lazy"
+          />
+          <article className="absolute bottom-0 left-0 right-0 p-2 lg:p-5 flex flex-col glass rounded-bl-3xl rounded-br-3xl duration-1000">
+            <h3 className="lg:text-2xl text-center">{achivement.title}</h3>
+            <p className="text-sm text-center">{achivement.description}</p>
+          </article>
+        </Link>
+      ))}
     </div>
   );
 };
---
+++ src/Utilities/variables/achivements.js
@@ -0,0 +1,20 @@
+export const achivements = [
+  {
+    id: 1,
+    title: "JPS Photo Contest 2020",
+    description: "It was an amazing contest by BAHARI PONNO",
+    photoUrl: "./images/achivements/arif-achivement-1.jpg",
+  },
+  {
+    id: 2,
+    title: "NGC Photo Contest",
+    description: "Handing over my two photograph by honorable Department Head",
+    photoUrl: "./images/achivements/arif-achivement-2.jpg",
+  },
+  {
+    id: 3,
+    title: "NSTU Photo Contest 2023",
+    description: "Thank you NSTU Photography Club",
+    photoUrl: "./images/achivements/arif-achivement-3.jpg",
+  },
+];
